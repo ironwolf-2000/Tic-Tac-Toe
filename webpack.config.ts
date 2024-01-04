59a05288e421c7530226af6ddd7ae27e36770648
Michael <103814817+ironwolf-2000@users.noreply.github.com>
2024-01-04 20:53:59 +0300

    feat: add assets

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,12 @@
-import path from 'path';
-import type { Configuration } from 'webpack';
+const path = require('path');
 
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config: Configuration = {
-    mode: (process.env.NODE_ENV as 'production' | 'development' | undefined) ?? 'development',
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
     entry: path.join(__dirname, 'src', 'index.tsx'),
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    },
     module: {
         rules: [
             {
@@ -20,11 +18,21 @@ const config: Configuration = {
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                test: /\.svg$/,
                 loader: 'url-loader',
                 options: { limit: 8192 },
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+        alias: {
+            '@assets': path.resolve(__dirname, 'src/assets'),
+        },
     },
     output: {
         filename: 'bundle.js',
@@ -36,5 +44,3 @@ const config: Configuration = {
         }),
     ],
 };
-
-export default config;
